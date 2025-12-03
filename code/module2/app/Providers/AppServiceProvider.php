4411ac9a472c02411c98d8f04b090dee47f9fd90
sparksj10 <sparks.josh03@gmail.com>
2025-12-02 23:14:55 -0500

    4

--- code/module2/app/Providers/AppServiceProvider.php
+++ code/module2/app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+    $this->routes(function () {
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    });
+}
 }
